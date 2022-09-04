332cc7d20aefc2e0b6c5d0088d3b36c5c7ba5cb8
D3SOX <nico@d3sox.me>
2022-09-04 14:01:08 +0200

    fix(deps): bump vitepress to 1.0.0-alpha.13

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -51,7 +51,6 @@ export default defineConfig({
 
         footer: {
             message: 'Made by D3SOX with ❤️',
-            copyright: '' // workaround for https://github.com/vuejs/vitepress/issues/1190
         },
     },
 });
